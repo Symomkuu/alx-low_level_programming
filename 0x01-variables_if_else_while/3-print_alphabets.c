dcb5ca0bd7830eef8a287b5d399f045cf4c474e3
Symomkuu <simonnjunge350@gmail.com>
2023-09-11 05:49:11 -0700

    tiring

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,19 @@
 int main(void)
 {
 	int n = 97;
-
+	
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	putchar('\n');
-	return (0);
+	int z = 65;
+
+	while (z <= 90)
+		{
+			putchar(z);
+			z++;
+		}
+		putchar('\n');
+		return (0);
 }

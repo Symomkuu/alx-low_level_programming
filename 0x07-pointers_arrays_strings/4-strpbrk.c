fe2a453a6f42de6dc6881df830c5190add104338
Symomkuu <simonnjunge350@gmail.com>
2023-09-25 14:21:17 -0700

    finding the first occurence of a substring

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strpbrk -  searches a string for any of a set of bytes 
+ * _strpbrk - searches a string for any of a set of bytes 
  * @s: input string
  * @accept: input
  * Return: s.

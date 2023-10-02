a4b6443d286db7fffb7e0b3b6159b0d9d17c6654
Symomkuu <simonnjunge350@gmail.com>
2023-10-02 11:39:03 -0700

    print string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	str = (char *)malloc(size * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;

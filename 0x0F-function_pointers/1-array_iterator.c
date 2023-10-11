ae3846ca0614ccf5d54ed961777b066cc8d4fe7d
Symomkuu <simonnjunge350@gmail.com>
2023-10-11 13:54:27 -0700

    iteration

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *array_iterator - iterates through all elements
+ *@array: the array
+ *size: size of the array
+ *action: the function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array != NULL && action != NULL)
+	{
+		for ( i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}

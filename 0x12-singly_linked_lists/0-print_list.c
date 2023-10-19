704b2bac9501de3380b18a792b361bf780fa8e0b
Symomkuu <simonnjunge350@gmail.com>
2023-10-19 15:08:37 -0700

    printing a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,45 +1,21 @@
+#include <stdio.h>
 #include "lists.h"
 /**
  * print_list - Print all the elements of a list_t list.
  * @h: A pointer to the head of the list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	char *str;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			_putchar('[');
-			_putchar('0');
-			_putchar(']');
-			_putchar(' ');
-			_putchar('(');
-			_putchar('n');
-			_putchar('i');
-			_putchar('l');
-			_putchar(')');
-		}
+			printf("[0] (nil)\n");
 		else
-		{
-			_putchar('[');
-			_putchar(h->len + '0');
-			_putchar(']');
-			_putchar(' ');
+			printf("[%u] %s\n", h->len, h->str);
 
-			str = h->str;
-			while (*str != '\0')
-			{
-				_putchar(*str);
-				str++;
-			}
-		}
-
-		_putchar('\n');
 		h = h->next;
 		count++;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <unistd.h>
 #include <stddef.h>
 /**
  * struct list_s - singly linked list
@@ -10,12 +10,12 @@
  *
  * Description: singly linked list node structure
  */
+int _putchar(char c);
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 }list_t;
-int _putchar(char c);
 size_t print_list(const list_t *h);
 #endif

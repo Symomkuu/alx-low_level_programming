4d98ca8eb0e4522baca958871ed41822ddbd505a
Symomkuu <simonnjunge350@gmail.com>
2023-10-24 13:11:46 -0700

    add node

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,21 @@
 #include "lists.h"
+
 /**
- *print_listint - prints all elements in alist
- *@h: pointer to first element
- *Return: count.
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	count = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count++;
 	}
+
 	return (count);
 }
